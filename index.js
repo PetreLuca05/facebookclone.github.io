0d2d1a186fcc426723e7921c85a91e72e15dc554
Petre Luca <petreluca1105@gmail.com>
2023-01-11 01:53:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,20 +15,3 @@ function ViewModeButton(name){
         btn.setAttribute('id', 'nav-bar-middle-tabs-selected');
     }
 }
-
-function CategoryButton(name){
-    let btn = document.getElementsByClassName(name)[0];
-
-    if(btn.id == 'selected')
-        return;
-
-    document.getElementsByClassName('middle-page-category-btn1')[0].setAttribute('id', '');
-    document.getElementsByClassName('middle-page-category-btn2')[0].setAttribute('id', '');
-    document.getElementsByClassName('middle-page-category-btn3')[0].setAttribute('id', '');
-
-    if(btn.id == ""){
-        btn.setAttribute('id', 'middle-page-first-selected');
-    }
-}
-
-
